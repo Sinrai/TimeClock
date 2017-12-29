923ba6f38dfb013d3675e2d38b1b16b857ff5f4a
Sinrai <32574358+Sinrai@users.noreply.github.com>
2017-12-29 18:19:09 +0100

    preparing bridge

--- Arduino/arduino.ino
+++ Arduino/arduino.ino
@@ -1,8 +1,11 @@
+#include <Bridge.h>
+
 const int lightresist = 2;
 const int led = 3;
 const int button = 4;
 
 void setup() {
+  //Bridge.begin();
   Serial.begin(9600); //Debug
   pinMode(lightresist, OUTPUT);
   digitalWrite(lightresist, LOW);
@@ -22,10 +25,8 @@ String readData() {
   String a;
   //for (int i = 5; i <= 13; i++) {
   for (int i = 13; i <= 13; i++) { //Debug
-    a = String(digitalRead(i));
-    // do stuff
+    a += String(digitalRead(i));
   }
-  //return stuff
   digitalWrite(led, LOW);
   digitalWrite(lightresist, LOW);
   return a;
@@ -35,8 +36,10 @@ void loop() {
   //Serial.println(digitalRead(button));
   if (digitalRead(button) == HIGH) {
     String output = readData();
-    Serial.println(output);
+    Serial.println(output); //Debug
     // transfer output to linux part
+
+    // end
     delay(1800);
   }
   delay(200);
--- Linux/timeclock.py
+++ Linux/timeclock.py
@@ -5,10 +5,13 @@ scope = ['https://spreadsheets.google.com/feeds']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('timeclockapi.json', scope)
 client = gspread.authorize(credentials)
 
+
 def loop():
-    #get input from arduino part
-    #put it in spreadsheet
-    pass
+    # get input from arduino part
+    debuginput = raw_input()
+    # put it in spreadsheet
+    print debuginput
+
 
 if __name__ == '__main__':
     while True:
