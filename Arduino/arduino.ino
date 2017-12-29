04c9fc45117074e8cc1e696ba9812d821c8602fc
Sinrai <32574358+Sinrai@users.noreply.github.com>
2017-12-29 14:51:23 +0100

    first circuit test
    Testcurcuit works:
    Serialmonitor shows if LDR "has" light or not if the button is pressed.

--- Arduino/arduino.ino
+++ Arduino/arduino.ino
@@ -1,25 +1,41 @@
-final int led = 3;
-final int button = 4;
+const int lightresist = 2;
+const int led = 3;
+const int button = 4;
 
 void setup() {
+  Serial.begin(9600); //Debug
+  pinMode(lightresist, OUTPUT);
+  digitalWrite(lightresist, LOW);
   pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
   pinMode(button, INPUT);
-  for (int i = 5; i <= 13; i++) {
+  //for (int i = 5; i <= 13; i++) {
+  for (int i = 13; i <= 13; i++) { //Debug
     pinMode(i, INPUT);
   }
 }
 
 String readData() {
-  for (int i = 5; i <= 13; i++) {
-    digitalRead(i);
+  digitalWrite(led, HIGH);
+  delay(100);
+  digitalWrite(lightresist, HIGH);
+  String a;
+  //for (int i = 5; i <= 13; i++) {
+  for (int i = 13; i <= 13; i++) { //Debug
+    a = String(digitalRead(i));
     // do stuff
   }
-  return stuff
+  //return stuff
+  digitalWrite(led, LOW);
+  digitalWrite(lightresist, LOW);
+  return a;
 }
 
 void loop() {
-  if (digitalRead(4) == HIGH) {
-    output = readData();
+  //Serial.println(digitalRead(button));
+  if (digitalRead(button) == HIGH) {
+    String output = readData();
+    Serial.println(output);
     // transfer output to linux part
     delay(1800);
   }
